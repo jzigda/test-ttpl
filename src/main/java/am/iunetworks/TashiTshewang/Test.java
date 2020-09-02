1e40c2a5f987497f55fc97b7ca4b77d079250f6e
ittashi <tashii12333@gmail.com>
2020-09-02 12:18:09 +0600

    commiting test

--- src/main/java/am/iunetworks/TashiTshewang/Test.java
+++ src/main/java/am/iunetworks/TashiTshewang/Test.java
@@ -3,5 +3,8 @@ package am.iunetworks.TashiTshewang;
 public class Test {
     public static void main(String[] args){
         System.out.println("Test");
+        System.out.println("Test");
+        System.out.println("Test");
+        System.out.println("Test");
     }
 }
