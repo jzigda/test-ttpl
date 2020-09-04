ab7316ed80d0277e11c768223e39c83b63293bf9
Dorji Norbu <dnorbu33@gmail.com>
2020-09-04 15:23:49 +0600

    little changes in question five

--- src/main/java/am/iunetworks/DorjiNorbu/questionFive/ToLowerCase.java
+++ src/main/java/am/iunetworks/DorjiNorbu/questionFive/ToLowerCase.java
@@ -7,12 +7,13 @@ public class ToLowerCase {
     public String toLowerCase(char[] chars) {
         StringBuffer stringBuffer = new StringBuffer();
         for (int i = 0; i < chars.length; i++) {
-            if (chars[i] >= 65 && chars[i] <= 90) {
-                stringBuffer.append((char) (chars[i] + 32));
+            if (chars[i] >= 65 && chars[i] <= 90) { //ASCII code of A(65) snd Z(90)
+                stringBuffer.append((char) (chars[i] + 32)); //32 is difference between A and a
             } else {
                 stringBuffer.append(chars[i]);
             }
         }
+
         return stringBuffer.toString();
     }
 }
